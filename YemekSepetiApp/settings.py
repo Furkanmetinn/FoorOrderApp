9adb8370d7499fbf06e5fd14aba1f92b75512965
Eda Çolak <edaacolakk1907@gmail.com>
2024-05-30 12:59:28 +0300

    eda

--- YemekSepetiApp/settings.py
+++ YemekSepetiApp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-@e4l!qu4fwgv4l*(lmd-hfw*)y&ktb_d5$vik97dpxxr5q90d$
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '10.0.0.2',
+    '10.0.2.2',
     '127.0.0.1',
     '10.0.3.84',
     '*'
